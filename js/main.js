4818378b84bef585b3e959067e24d4ea70c57c75
Stankovic Ivan <stankovicmivan@gmail.com>
2018-11-09 22:51:32 +0100

    js setup

---
+++ js/main.js
@@ -0,0 +1,36 @@
+window.sr = ScrollReveal();
+sr.reveal ('.navbar', {
+    duration: 2000,
+    origin: 'bottom'
+});
+
+sr.reveal ('.showcase-left', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+});
+sr.reveal ('.showcase-right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '500px'
+});
+sr.reveal ('.showcase-btn', {
+    duration: 2000,
+    delay: 1500,
+    origin: 'bottom',
+});
+sr.reveal ('#about div', {
+    duration: 2000,
+    origin: 'bottom',
+});
+sr.reveal ('.left', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+});
+sr.reveal ('.right', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px',
+    viewFactor: 0.2
+});
